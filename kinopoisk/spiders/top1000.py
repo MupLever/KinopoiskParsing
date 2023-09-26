5b347b4e598fff74bef7a947cd2cce4c0a6b5dfd
shlyzhas <8willox8@gmail.com>
2023-09-26 18:13:53 +0300

    добавлен паук с методами парсинга

---
+++ kinopoisk/spiders/top1000.py
@@ -0,0 +1,26 @@
+from typing import Iterable
+import scrapy
+from scrapy.http import Request
+
+
+class Top1000Spider(scrapy.Spider):
+    name = "top1000"
+    allowed_domains = ["kinopoisk.ru"]
+    start_urls = ["https://www.kinopoisk.ru/lists/movies/top_1000/"]
+    count_pages = 20
+
+    def parse(self, response):
+        for page in range(1, self.count_pages + 1):
+            url = f"https://www.kinopoisk.ru/lists/movies/top_1000/?page={page}"
+            yield response.follow(url, callback=self.parse)
+
+    def parse_pages(self, response):
+        for i in range(50):
+            yield {
+                'name_of_the_movie': response.css('span.styles_mainTitle__IFQyZ.styles_activeMovieTittle__kJdJj::text')[i].get(),
+                'year': response.css('span.desktop-list-main-info_secondaryText__M_aus::text')[i].get().split(',')[0],
+                'country': response.css('span.desktop-list-main-info_truncatedText__IMQRP::text')[i].get().split()[0],
+                'producer': ' '.join(response.css('span.desktop-list-main-info_truncatedText__IMQRP::text')[i].get().split()[-2:]),
+                'raiting': response.css('span.styles_kinopoiskValuePositive__vOb2E styles_kinopoiskValue__9qXjg::text')[i].get(),
+                'link': ' '
+            }
