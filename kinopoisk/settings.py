a19ba6c95284b12e2f6efc9d8baae6eff0a1c1c1
shlyzhas <8willox8@gmail.com>
2023-09-28 20:17:43 +0300

    умень. кол-во конкурентных запросов и ув. таймаут

--- kinopoisk/settings.py
+++ kinopoisk/settings.py
@@ -19,12 +19,12 @@ NEWSPIDER_MODULE = "kinopoisk.spiders"
 # ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 8
+CONCURRENT_REQUESTS = 1
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 0.8
+DOWNLOAD_DELAY = 30
 # The download delay setting will honor only one of:
 # CONCURRENT_REQUESTS_PER_DOMAIN = 16
 # CONCURRENT_REQUESTS_PER_IP = 16
